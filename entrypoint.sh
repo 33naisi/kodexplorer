2c2961a17eaf40adb57e0a1bb588dd8d36988abd
33naisi <55055259+33naisi@users.noreply.github.com>
2020-09-07 17:22:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ if [ "$1" = 'php' ] && [ "$(id -u)" = '0' ]; then
 fi
 if [ ! -e '/var/www/html/index.php' ]; then
     cp -a /usr/src/kodexplorer/* /var/www/html/
-    echo -e '<?php\n//分片上传: 每个切片5M,需要php.ini 中upload_max_filesize大于此值\n$GLOBALS['config']['settings']['updloadChunkSize'] = 1024*1024*5;\n//上传并发数量; 推荐15个并发;\n$GLOBALS['config']['settings']['updloadThreads'] = 15;' > /var/www/html/config/setting_user.php
 fi
 
 exec "$@"
